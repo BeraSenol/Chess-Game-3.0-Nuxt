51c5cb264da64b178163c239229a7c0e872e8793
Bera Senol <bleuberry2be@icloud.com>
2025-01-02 18:20:21 +0100

    Indicators

--- components/ChessBoard.vue
+++ components/ChessBoard.vue
@@ -1,12 +1,12 @@
 <template>
   <UContainer>
     <div v-for="ranks, rank in chess.board()" class="flex">
-      <div v-for="tile, file in ranks" :key="tile?.square" :id="generateId(file, rank)" class="u-tile-size"
+      <div v-for="tile, file in ranks" :key="tile?.square" :id="getId(file, rank)" class="u-tile-size relative"
         :class="(file + rank) % 2 === 0 ? 'u-tile-light' : 'u-tile-dark'">
-        <p class="text-black absolute">{{ generateId(file, rank) }}</p>
-        <div class="u-tile-size absolute chess-tile highlighted"></div>
+        <p class="text-black absolute">{{ getId(file, rank) }}</p>
         <NuxtImg v-if="tile" class="u-tile-size" :src="`/pieces/${tile.type}${tile.color}.svg`"
           :alt="`${tile.type}${tile.color}.svg`" @click="hightlightMoves(tile.square)" />
+        <Indicator :id="`${getId(file, rank)}-indicator`"/>
       </div>
     </div>
   </UContainer>
@@ -18,72 +18,34 @@ import { Chess, type Square } from 'chess.js'
 
 const chess = new Chess();
 
-const generateId = (file: number, rank: number): string => {
-  let id = '';
-  switch (file) {
-    case 0: id = 'a';
+const getId = (fileIndex: number, rankIndex: number): string => {
+  let fileLabel: string = '';
+  console.log(chess.moves({square: 'b1'}))
+  switch (fileIndex) {
+    case 0: fileLabel = 'a';
       break;
-    case 1: id = 'b';
+    case 1: fileLabel = 'b';
       break;
-    case 2: id = 'c';
+    case 2: fileLabel = 'c';
       break;
-    case 3: id = 'd';
+    case 3: fileLabel = 'd';
       break;
-    case 4: id = 'e';
+    case 4: fileLabel = 'e';
       break;
-    case 5: id = 'f';
+    case 5: fileLabel = 'f';
       break;
-    case 6: id = 'g';
+    case 6: fileLabel = 'g';
       break;
-    case 7: id = 'h';
-      break;
-  }
-
-  switch (rank) {
-    case 0: id += '8';
-      break;
-    case 1: id += '7';
-      break;
-    case 2: id += '6';
-      break;
-    case 3: id += '5';
-      break;
-    case 4: id += '4';
-      break;
-    case 5: id += '3';
-      break;
-    case 6: id += '2';
-      break;
-    case 7: id += '1';
+    case 7: fileLabel = 'h';
       break;
   }
-  return `${id}`;
+  return `${fileLabel}${8 - rankIndex}`;
 }
 
 const hightlightMoves = (target: Square) => {
   for (let i = 0; i < chess.moves({ square: target }).length; i++) {
-    const square = document.getElementById(chess.moves({ square: target })[i]);
-    square?.classList.add('bg-black');
+    const indicator = document.getElementById(`${chess.moves({ square: target })[i]}-indicator`);
+    indicator?.classList.add('highlighted');
   }
 }
 </script>
-
-<style scoped>
-/* .chess-tile {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-} */
-
-.chess-tile.highlighted::before {
-  content: '';
-  width: 30px;
-  /* Adjust size */
-  height: 30px;
-  /* Adjust size */
-  background-color: rgba(0, 0, 0, 0.20);
-  border-radius: 50%;
-  pointer-events: none;
-  /* Prevent interference with clicks */
-}
-</style>
---
+++ components/Indicator.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="u-tile-size absolute indicator"></div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+.indicator {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.indicator.highlighted::before {
+  content: '';
+  width: 30px;
+  height: 30px;
+  background-color: rgba(0, 0, 0, 0.20);
+  border-radius: 50%;
+  pointer-events: none;
+}
+</style>
