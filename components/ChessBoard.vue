ac3d40b650b8548b96c9a600a95a1f2159c64e97
Bera Senol <bleuberry2be@icloud.com>
2025-01-02 21:17:18 +0100

    Move indicator bugfic

--- components/ChessBoard.vue
+++ components/ChessBoard.vue
@@ -1,51 +1,49 @@
 <template>
-  <UContainer>
-    <div v-for="ranks, rank in chess.board()" class="flex">
-      <div v-for="tile, file in ranks" :key="tile?.square" :id="getId(file, rank)" class="u-tile-size relative"
-        :class="(file + rank) % 2 === 0 ? 'u-tile-light' : 'u-tile-dark'">
-        <p class="text-black absolute">{{ getId(file, rank) }}</p>
-        <NuxtImg v-if="tile" class="u-tile-size" :src="`/pieces/${tile.type}${tile.color}.svg`"
-          :alt="`${tile.type}${tile.color}.svg`" @click="hightlightMoves(tile.square)" />
-        <Indicator :id="`${getId(file, rank)}-indicator`"/>
-      </div>
-    </div>
-  </UContainer>
+	<UContainer>
+		<div v-for="ranks, rank in chess.board()" class="flex">
+			<div v-for="tile, file in ranks" :key="tile?.square" :id="getId(file, rank)" class="u-tile-size relative"
+				:class="(file + rank) % 2 === 0 ? 'u-tile-light' : 'u-tile-dark'">
+				<p class="text-black absolute">{{ getId(file, rank) }}</p>
+				<NuxtImg v-if="tile" class="u-tile-size" :src="`/pieces/${tile.type}${tile.color}.svg`"
+					:alt="`${tile.type}${tile.color}.svg`" :id="`${getId(file, rank)}-piece`"
+					@click="hightlightMoves(<Square>getId(file, rank))" />
+				<Indicator :id="`${getId(file, rank)}-indicator`" />
+			</div>
+		</div>
+	</UContainer>
 </template>
 
 <script lang="ts" setup>
-import type { _square } from '#tailwind-config/theme/aspectRatio';
 import { Chess, type Square } from 'chess.js'
 
 const chess = new Chess();
 
 const getId = (fileIndex: number, rankIndex: number): string => {
-  let fileLabel: string = '';
-  console.log(chess.moves({square: 'b1'}))
-  switch (fileIndex) {
-    case 0: fileLabel = 'a';
-      break;
-    case 1: fileLabel = 'b';
-      break;
-    case 2: fileLabel = 'c';
-      break;
-    case 3: fileLabel = 'd';
-      break;
-    case 4: fileLabel = 'e';
-      break;
-    case 5: fileLabel = 'f';
-      break;
-    case 6: fileLabel = 'g';
-      break;
-    case 7: fileLabel = 'h';
-      break;
-  }
-  return `${fileLabel}${8 - rankIndex}`;
+	let fileLabel: string = '';
+	switch (fileIndex) {
+		case 0: fileLabel = 'a';
+			break;
+		case 1: fileLabel = 'b';
+			break;
+		case 2: fileLabel = 'c';
+			break;
+		case 3: fileLabel = 'd';
+			break;
+		case 4: fileLabel = 'e';
+			break;
+		case 5: fileLabel = 'f';
+			break;
+		case 6: fileLabel = 'g';
+			break;
+		case 7: fileLabel = 'h';
+			break;
+	}
+	return `${fileLabel}${8 - rankIndex}`;
 }
 
-const hightlightMoves = (target: Square) => {
-  for (let i = 0; i < chess.moves({ square: target }).length; i++) {
-    const indicator = document.getElementById(`${chess.moves({ square: target })[i]}-indicator`);
-    indicator?.classList.add('highlighted');
-  }
+const hightlightMoves = (tile: Square) => {
+	for (let i = 0; i < chess.moves({ square: tile }).length; i++) {
+		document.getElementById(`${chess.moves({ square: tile, verbose: true })[i].to}-indicator`)?.classList.add('highlighted')
+	}
 }
 </script>
